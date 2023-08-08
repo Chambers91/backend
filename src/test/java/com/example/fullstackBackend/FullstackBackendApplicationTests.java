e889e5afa6437ce2e6b0e92737f778c3a89fccb4
Terence Chambers <terencechambers91@gmail.com>
2023-08-08 14:49:26 -0400

    Initial Backend Commit

---
+++ src/test/java/com/example/fullstackBackend/FullstackBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
