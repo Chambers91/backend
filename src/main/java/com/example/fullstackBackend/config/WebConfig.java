158dddd8738b21d41633f4c7bc4a58135eab8fdc
Terence Chambers <terencechambers91@gmail.com>
2023-08-10 14:17:46 -0400

    CORS implementation started

--- src/main/java/com/example/fullstackBackend/config/WebConfig.java
+++ src/main/java/com/example/fullstackBackend/config/WebConfig.java
@@ -1,2 +1,45 @@
-package com.example.fullstackBackend.config;public class WebConfig {
+package com.example.fullstackBackend.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.Arrays;
+
+@SuppressWarnings("ALL")
+@Configuration
+@EnableWebMvc
+public class WebConfig {
+
+    //CORS connection with the backend code to the frontend code
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:4200");
+        config.setAllowedHeaders(Arrays.asList(
+                HttpHeaders.AUTHORIZATION,
+                HttpHeaders.CONTENT_TYPE,
+                HttpHeaders.ACCEPT));
+        //backend must accept methods
+        config.setAllowedMethods(Arrays.asList(
+                HttpMethod.GET.name(),
+                HttpMethod.POST.name(),
+                HttpMethod.PUT.name(),
+                HttpMethod.DELETE.name()));
+        config.setMaxAge(3600L);
+        //configuration added to all
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean (new CorsFilter(source));
+        bean.setOrder(-100);
+        // should be set order to -100 because we need to CorsFilter before SpringSecurityFilter
+        return bean;
+    }
 }
